60b29f2bc5c8c593e45e3062770696f79aadb09d
michael-bailey <mickyb18a@gmail.com>
2021-01-28 15:17:42 +0000

    added transaction model and balance function

--- models.js
+++ models.js
@@ -1,36 +1,62 @@
-const {Sequelize, Model, DataTypes} = require('sequelize')
-const path = require('path')
+const { Sequelize, Model, DataTypes } = require("sequelize");
+const path = require("path");
 
 const connectionSettings = {
-    test: {dialect: 'sqlite', storage: 'sqlite::memory:'},
-    dev: {dialect: 'sqlite', storage: path.join(__dirname, 'data.db')},
-    production: {dialect: 'postgres', protocol: 'postgres'}
+  test: { dialect: "sqlite", storage: "sqlite::memory:" },
+  dev: { dialect: "sqlite", storage: path.join(__dirname, "data.db") },
+  production: { dialect: "postgres", protocol: "postgres" },
+};
+
+const sequelize =
+  process.env.NODE_ENV === "production"
+    ? new Sequelize(
+        process.env.DATABASE_URL,
+        connectionSettings[process.env.NODE_ENV]
+      )
+    : new Sequelize(connectionSettings[process.env.NODE_ENV]);
+
+class User extends Model {
+  async getBalance() {
+    this.transactions.reduce();
+  }
 }
-
-const sequelize = process.env.NODE_ENV === 'production'
-    ? new Sequelize(process.env.DATABASE_URL, connectionSettings[process.env.NODE_ENV])
-    : new Sequelize(connectionSettings[process.env.NODE_ENV])
-
-class User extends Model {}
-User.init({
+User.init(
+  {
     name: DataTypes.STRING,
     email: DataTypes.STRING,
-    balance: DataTypes.FLOAT
-}, {sequelize})
+    balance: DataTypes.FLOAT,
+  },
+  { sequelize }
+);
 
-// class Transaction extends Model {}
-// Transaction.init({
-//     sender: DataTypes.STRING,
-//     recipient: DataTypes.STRING,
-//     amount: DataTypes.FLOAT
-// }, {sequelize})
+// MARK: - transaction model
 
-// UserTransaction = sequelize.define('user_project')
-// User.belongsToMany(Transaction, { through: UserTransaction })
-// Transaction.belongsToMany(User, { through: UserTransaction })
+class Transaction extends Model {}
+Transaction.init(
+  {
+    transactionID: {
+      type: DataTypes.UUIDV4,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      unique: true,
+    },
+
+    sender: { type: DataTypes.STRING, allowNull: false },
+    senderDomain: { type: DataTypes.STRING, allowNull: false },
+
+    recipient: { type: DataTypes.STRING, allowNull: false },
+    recipientDomain: { type: DataTypes.STRING, allowNull: false },
+
+    amount: { type: DataTypes.FLOAT, validate: { min: 0.01 } },
+  },
+  { sequelize }
+);
+
+User.hasMany(Transaction);
+Transaction.belongsTo(User);
 
 module.exports = {
-    User,
-    // Transaction,
-    sequelize
-}
+  User,
+  Transaction,
+  sequelize,
+};
