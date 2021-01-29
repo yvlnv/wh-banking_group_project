d4b4f210c317099ccbd822c2518930081e127dc4
michael-bailey <mickyb18a@gmail.com>
2021-01-29 10:33:40 +0000

    removed transaction controller

--- Controllers/api/TransactionController.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-
-// defining transaction endpoint Controller
-const TransactionController = Router();
-
-// external actions
-TransactionController.route("/pay").post(async (req, res) => {});
-
-module.exports = TransactionController;
